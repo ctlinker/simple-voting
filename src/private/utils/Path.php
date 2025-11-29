dda2e6d04197eb79a6cb3df86e274e5ab09d834f
Ctlinker <166874035+Ctlinker@users.noreply.github.com>
2025-11-29 20:48:48 +0000

    feat: added `Utils` namespace currently holding a `Path` class wrapper

---
+++ src/private/utils/Path.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Utils;
+
+class Path{
+    public static function resolvePrivatePath(string $path){
+        return __DIR__ . "/" . ".." . "/" . $path;
+    }
+
+    public static function resolvePublicPath(string $path){
+        return __DIR__ . "/" . ".." . "/" . "public" . "/" . $path;
+    }
+}
